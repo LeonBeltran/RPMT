7288a15c37a78bcbf65219c4cdf8c97a11ef676d
LeonBeltran <enbeltranii@gmail.com>
2024-07-09 15:28:17 +0800

    Added project addition html page

--- rpmt.py
+++ rpmt.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Home Page
+# ----------------------------------------------------------------------------------------------
 @app.get("/")
 @app.get("/home")
 def home():
     return render_template("home.html")
 
 # Projects Page
+# ----------------------------------------------------------------------------------------------
 dummy_data = [
     {
         "ID": 0,
@@ -48,6 +50,7 @@ def show_project(paper_id):
     return render_template("projectpage.html", project=dummy_data[paper_id])
 
 # Admin: Login Page
+# ----------------------------------------------------------------------------------------------
 @app.get("/login")
 def login_get():
     return render_template("login.html")
@@ -56,28 +59,33 @@ def login_post():
     return render_template("login.html")
 
 # Admin: Admin Area
+# ----------------------------------------------------------------------------------------------
 @app.get("/admin/")
 def admin():
     return render_template("admin.html")
 
 # Admin: Generate Projects Report
+# ----------------------------------------------------------------------------------------------
 @app.get("/admin/report")
 def show_projects_report():
     return "<p>Reports Generation</p>"
 
 # Admin: Adding Projects
+# ----------------------------------------------------------------------------------------------
 @app.get("/admin/add/")
 @app.get("/admin/add/info")
 def add_project_info_get():
-    return "<p>GET Add Project Info </p>"
+    return render_template("addproject.html")
+
 @app.post("/admin/add/")
 @app.post("/admin/add/info")
 def add_project_info_post():
-    return "<p>POST Add Project Info</p>"
+    return render_template("addproject.html")
 
 @app.get("/admin/add/authors")
 def add_project_authors_get():
     return "<p>GET Add Project Authors </p>"
+
 @app.post("/admin/add/authors")
 def add_project_authors_post():
     return "<p>POST Add Project Authors</p>"
@@ -85,22 +93,27 @@ def add_project_authors_post():
 @app.get("/admin/add/editors")
 def add_project_editors_get():
     return "<p>GET Add Project Editors </p>"
+
 @app.post("/admin/add/editors")
 def add_project_editors_post():
     return "<p>POST Add Project Editors</p>"
 
 # Admin: Deleting Projects
+# ----------------------------------------------------------------------------------------------
 @app.get("/admin/delete/")
 def admin_delete_projects():
     return "<p>Deletable Projects</p>"
+
 @app.get("/admin/delete/<int:paper_id>")
 def delete_project(paper_id):
     return f"<p>Deleting ID: {paper_id}</p>"
 
 # Admin: Editing Projects
+# ----------------------------------------------------------------------------------------------
 @app.get("/admin/edit/")
 def admin_edit_projects():
     return "<p>Editable Projects</p>"
+
 @app.get("/admin/edit/<int:paper_id>")
 def edit_project(paper_id):
     return f"<p>Editing ID: {paper_id}</p>"
