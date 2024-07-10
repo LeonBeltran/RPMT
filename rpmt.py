b80603b88e7967f20b90df6582328ef18bdeadf2
LeonBeltran <enbeltranii@gmail.com>
2024-07-10 16:04:40 +0800

    Added data models

--- rpmt.py
+++ rpmt.py
@@ -1,15 +1,87 @@
 from flask import Flask, render_template
-from Scratch.dummydata import dummy_data
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
+from Scratch.dummydata import dummy_data
+
 # Setup
 # ----------------------------------------------------------------------------------------------
-app = Flask(__name__)
-
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
+app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+# Database
+# ----------------------------------------------------------------------------------------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    role = db.Column(db.String(8), nullable=False)
+    projects = db.relationship('Project', backref='creator', lazy=True)
+    
+    def __repr__(self):
+        return f"User {self.id}: {self.username} | {self.email}"
+    
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    projects = db.relationship('AuthorProject', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"Author {self.id}: {self.name}"
+
+class Editor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    projects = db.relationship('EditorProject', backref='editor', lazy=True)
+    
+    def __repr__(self):
+        return f"Editor {self.id}: {self.name}"
+    
+class AuthorProject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    
+class EditorProject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('editor.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(256), nullable=False)
+    authors = db.relationship('AuthorProject', backref='project', lazy=True)
+    abstract = db.Column(db.String(512), nullable=True)
+    type = db.Column(db.String(64), nullable=False)
+    date_published = db.Column(db.Date, nullable=False)
+    publication_name = db.Column(db.String(128), nullable=False)
+    publisher = db.Column(db.String(64), nullable=False)
+    publisher_type = db.Column(db.String(32), nullable=False)
+    publisher_location = db.Column(db.String(16), nullable=False)
+    editors = db.relationship('EditorProject', backref='project', lazy=True)
+    vol_issue_no = db.Column(db.Integer, nullable=False)
+    doi_url = db.Column(db.String(256), unique=True, nullable=False)
+    isbn_issn = db.Column(db.String(4), nullable=False)
+    web_of_science = db.Column(db.Boolean, nullable=False)
+    elsevier_scopus = db.Column(db.Boolean, nullable=False)
+    elsevier_sciencedirect = db.Column(db.Boolean, nullable=False)
+    pubmed_medline = db.Column(db.Boolean, nullable=False)
+    ched_recognized = db.Column(db.Boolean, nullable=False)
+    other_database = db.Column(db.String(128), nullable=False)
+    publication_proof = db.Column(db.String(32), nullable=False, default='none.png')
+    citations = db.Column(db.Integer, nullable=False)
+    utilization_proof = db.Column(db.String(32), nullable=False, default='none.png')
+    
+    def __repr__(self):
+        return f"Project {self.id}: {self.title}"
 
 # Home Page
 # ----------------------------------------------------------------------------------------------
