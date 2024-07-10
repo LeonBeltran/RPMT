a933e8df89500100321b6598e2104d4aaf7850eb
LeonBeltran <enbeltranii@gmail.com>
2024-07-10 16:34:42 +0800

    Cleaning

--- rpmt/routes.py
+++ rpmt/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from rpmt import app
 
-from Scratch.dummydata import dummy_data
+from _scratch.dummydata import dummy_data
 
 # Home Page
 # ----------------------------------------------------------------------------------------------
