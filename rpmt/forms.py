31d71845ef3fda6231c85704f061b19817ba5db2
LeonBeltran <enbeltranii@gmail.com>
2024-07-15 13:08:09 +0800

    Add project form connected, to make backend and test

--- rpmt/forms.py
+++ rpmt/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, DateField, SubmitField
+from wtforms import StringField, IntegerField, BooleanField, DateField, SubmitField, TextAreaField, SelectField, FileField
 from wtforms.validators import DataRequired, Length, URL, Optional
 
 class LoginForm(FlaskForm): 
@@ -13,8 +13,10 @@ class LoginForm(FlaskForm):
 class ProjectForm(FlaskForm):
     title = StringField('Title',
                         validators=[DataRequired(), Length(max=256)])
-    abstract = StringField('Abstract',
+    abstract = TextAreaField('Abstract',
                            validators=[Optional(), Length(max=512)])
+    authors = StringField('Authors',
+                          validators=[DataRequired(), Length(max=512)])
     type = StringField('Type',
                        validators=[DataRequired(), Length(max=64)])
     date_published = DateField('Date Published',
@@ -27,12 +29,15 @@ class ProjectForm(FlaskForm):
                                  validators=[DataRequired(), Length(max=32)])
     publisher_location = StringField('Publisher Location',
                                      validators=[DataRequired(), Length(max=16)])
+    editors = StringField('Editors',
+                          validators=[DataRequired(), Length(max=512)])
     vol_issue_no = IntegerField('Volume/Issue No',
                                 validators=[DataRequired()])
     doi_url = StringField('DOI URL',
                           validators=[DataRequired(), URL(), Length(max=256)])
-    isbn_issn = StringField('ISBN/ISSN',
-                            validators=[DataRequired(), Length(max=4)])
+    isbn_issn = SelectField('ISBN/ISSN',
+                            choices=[('NONE', 'None'), ('ISBN', 'ISBN'), ('ISSN', 'ISSN')],
+                            validators=[DataRequired()])
     web_of_science = BooleanField('Web of Science')
     elsevier_scopus = BooleanField('Elsevier Scopus')
     elsevier_sciencedirect = BooleanField('Elsevier ScienceDirect')
@@ -40,10 +45,10 @@ class ProjectForm(FlaskForm):
     ched_recognized = BooleanField('CHED Recognized')
     other_database = StringField('Other Database',
                                  validators=[Optional(), Length(max=128)])
-    publication_proof = StringField('Publication Proof',
-                                    validators=[Optional(), Length(max=32)], default='none.png')
+    publication_proof = FileField('Publication Proof',
+                                    validators=[Optional()])
     citations = IntegerField('Citations',
                              validators=[DataRequired()])
-    utilization_proof = StringField('Utilization Proof',
-                                    validators=[Optional(), Length(max=32)], default='none.png')
+    utilization_proof = FileField('Utilization Proof',
+                                    validators=[Optional()])
     submit = SubmitField('Submit')
