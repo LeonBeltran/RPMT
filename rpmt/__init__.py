28750ba6b34c5b948d32c8238daabba92c84ebb0
LeonBeltran <enbeltranii@gmail.com>
2024-07-29 14:17:59 +0800

    Made dotenv loading more universal

--- rpmt/__init__.py
+++ rpmt/__init__.py
@@ -5,12 +5,20 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 from dotenv import load_dotenv
+from pathlib import Path
+import sys
 import os
 
 # Setup
 # ----------------------------------------------------------------------------------------------
-load_dotenv()
+if getattr(sys, 'frozen', False):
+    base_path = Path(sys._MEIPASS)
+else:
+    base_path = Path(__file__).parent
+dotenv_path = base_path / '.env'
+load_dotenv(dotenv_path)
 SECRET_KEY = os.getenv("SECRET_KEY")
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['UPLOAD_FOLDER'] = 'rpmt/uploads/'
