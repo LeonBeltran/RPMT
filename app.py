1c1dce5a3dc43d77544cf6adb4e5e86ce30de0c2
LeonBeltran <enbeltranii@gmail.com>
2024-08-06 09:32:20 +0800

    Used waitress server

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from rpmt import app
+from waitress import serve
 import webbrowser
 import threading
 
 def open_browser():
-    webbrowser.open_new('http://127.0.0.1:5000')  # Replace with your desired address
+    webbrowser.open_new('http://127.0.0.1:8080')  # Replace with your desired address
 
 if __name__ == '__main__':
     threading.Timer(1, open_browser).start()  # Open browser after 1 second
-    app.run()
+    serve(app, host="0.0.0.0", port=8080)
